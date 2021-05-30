814d76125872a80cca1a0ecb5001bdb2346e752c
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-30 15:13:01 +0900

    fbv로 블로그 포스트 목록 페이지 만들기

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'myblog/index.html',
+        {'posts': posts}
+    )
