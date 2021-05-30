cd06a349090b955508bdd8e184ede30b6bb9cfc8
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-30 19:46:55 +0900

    첨부파일이 있을 경우 상세페이지에 다운로드 버튼 출력하기

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 
 class Post(models.Model):
@@ -19,3 +20,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/myblog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
