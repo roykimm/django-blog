ca86f7fd8ff2fb707e2ec7caaf175812a8d141db
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-30 16:25:15 +0900

    cbv로 블로그 포스트 상세 페이지 만들기

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_pages),
-    path('', views.index)
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    #path('<int:pk>/', views.single_post_pages),
+    #path('', views.index)
+
 ]
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'myblog/index.html' # 이게 없어도 post_list로 html작업을 하면 보임
 
-    return render(
-        request,
-        'myblog/index.html',
-        {'posts': posts}
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+
+#     return render(
+#         request,
+#         'myblog/index.html',
+#         {'posts': posts}
+#     )
 
 
-def single_post_pages(request, pk):
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'myblog/single_post_page.html'
+# def single_post_pages(request, pk):
+#     post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'myblog/single_post_page.html',
-        {'post': post}
-    )
+#     return render(
+#         request,
+#         'myblog/single_post_page.html',
+#         {'post': post}
+#     )
