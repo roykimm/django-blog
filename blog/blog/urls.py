3e19bb52e8cf0bf044e25fa6e68e7166ebeeec05
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-30 16:16:52 +0900

    대문 체이지와 자기소개

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('myblog/', include('myblog.urls')),
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
 ]
