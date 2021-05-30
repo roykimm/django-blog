234dc01c40321819e189f036c9efddaea4c14d14
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-30 23:08:26 +0900

    삭제된 작성자는 None으로 표기

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -15,7 +15,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     # author : 추후 작성예정
 
