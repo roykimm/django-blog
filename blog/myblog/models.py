78cd168877eae1f6ff960f278496772ee9315429
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-31 00:02:01 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 import os
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True,
+                            blank=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     hook_text = models.CharField(max_length=200, blank=True)
@@ -17,6 +29,8 @@ class Post(models.Model):
 
     #author = models.ForeignKey(User, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        Category, null=True, on_delete=models.SET_NULL)
 
     # author : 추후 작성예정
 
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(
+            category=None).count()
+
+        return context
+
     # template_name = 'myblog/index.html' # 이게 없어도 post_list로 html작업을 하면 보임
 
 # def index(request):
