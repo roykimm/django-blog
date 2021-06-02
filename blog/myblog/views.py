ddf0640f15724df3e1229faf6eb8b4dcee0bade3
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-02 23:03:12 +0900

    로그인 사용자에게만 페이지 열어주기

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else :
+            return redirect('/blog/')
 
 class PostList(ListView):
     model = Post
