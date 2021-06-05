c944fe6ffe96f9f88b60347b55167b6fbab6cd32
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-05 16:04:06 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/myblog/admin.py
+++ blog/myblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from markdownx.admin import MarkdownxModelAdmin
 
 
@@ -13,3 +13,4 @@ class TagAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
