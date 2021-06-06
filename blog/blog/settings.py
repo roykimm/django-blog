5716cb38a449f859368fd918c37cbe6a05482b92
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-06 23:33:47 +0900

    t

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -92,9 +92,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('SQL_DATABASE',os.path.join(BASE_DIR,'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER','user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD','password'),
+        'HOST': os.environ.get('SQL_HOST','localhost'),
+        'PORT': os.environ.get('SQL_PORT','5432'),
     }
 }
 
@@ -136,6 +144,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
