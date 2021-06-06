32770c8018d0a22d900561c498c377070b141c66
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-06 18:01:33 +0900

    도커에 장고만 담기

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x6o82@8!ao)6v9pgp&qx#_hqp30%+kp_)6m-w!!d37p-#ben0v'
+#SECRET_KEY = 'django-insecure-x6o82@8!ao)6v9pgp&qx#_hqp30%+kp_)6m-w!!d37p-#ben0v'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-x6o82@8!ao)6v9pgp&qx#_hqp30%+kp_)6m-w!!d37p-#ben0v')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG',1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else :
+    ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
