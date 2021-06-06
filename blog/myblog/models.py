98738fc28b53be773c2810b179a70ca47d0d74ad
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-06 11:38:32 +0900

    사용자 아바타 보여주기

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -78,3 +78,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return "http://placehold.it/50x50"
