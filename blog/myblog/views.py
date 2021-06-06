a9b1fb23678f0f2e62cfd45fcf9b403ac2c3169c
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-06 11:30:11 +0900

    검색기능 구현

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
+from django.db.models import Q
 
 
 
@@ -107,6 +108,22 @@ class PostList(ListView):
 #         'myblog/index.html',
 #         {'posts': posts}
 #     )
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+        return context
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
